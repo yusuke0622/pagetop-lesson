67e162bc5e60e8ef3d6beb97144fa88f2a318493
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-11-05 00:43:54 +0000

    Update script_js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function() {
-    $('#back a').on('click', function(event){
-        $('body, html').animate({
-            scrollTop:0
-        }, 800);
-        event.preventDefault();
+    $('#back a').on('click', function(event){   //#back内のaタグがクリックされたときの処理
+        $('body, html').animate({  //body要素またはhtml要素(ユーザの利用環境によって指定対象の要素が変化するため)
+            scrollTop:0   //最上部に移動する
+        }, 800);   //800ミリ秒間（0.8秒間）かけてページの最上部まで移動する
+        event.preventDefault();  
     });
 });
